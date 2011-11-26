987fd5099eb6f1282286d24dd2ea21844215c9c0
DooMLoRD <metoo.mememe@gmail.com>
2011-11-26 17:23:10 +0530

    increased VM MAX READAHEAD to 512KB

--- kernel/include/linux/mm.h
+++ kernel/include/linux/mm.h
@@ -1174,7 +1174,7 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	128	/* kbytes */
+#define VM_MAX_READAHEAD	512	/* kbytes */
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
 
 int do_page_cache_readahead(struct address_space *mapping, struct file *filp,
